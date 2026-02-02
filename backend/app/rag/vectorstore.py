2f8931184c6dc12436dc1105481c291ec75d82e9
MohammadHasan2 <mohammad.hasan2004.30.05@gmail.com>
2026-02-02 23:26:27 +0200

    done

--- backend/app/rag/vectorstore.py
+++ backend/app/rag/vectorstore.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
 import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
